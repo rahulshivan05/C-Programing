f5977f56810f612f023b34696d2ffef39352c4fa
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-10-12 09:58:05 +0530

    add the number

---
+++ qus1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter first Number ");
+    scanf("%d", &a);
+
+    printf("Enter Second Number ");
+    scanf("%d", &b);
+
+    printf("The output is %d ", a + b);
+    return 0;
+}
