8e46aab5043ff999bacc0479f28d56188fa5180b
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-11-28 15:31:40 +0530

    void pointer in C

--- void-pointer.c
+++ void-pointer.c
@@ -9,6 +9,6 @@ int main(int argc, char const *argv[])
     printf("The value of a is %d\n", *((int *)ptr));
     ptr = &b;
     printf("The value of b is %f\n", *((float *)ptr));
-    // printf("The value of b is %f\n", b);
+    printf("The value of b is %f\n", b);
     return 0;
 }
