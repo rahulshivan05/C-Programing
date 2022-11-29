a32d18ef9715d0678d76c8e638602d62ec8069e0
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-11-29 10:57:29 +0530

    Fix the bug in this Code

--- while-test.c
+++ while-test.c
@@ -8,14 +8,14 @@ int main()
     while (i < 3)
     {
         printf("Enter the value of a\n");
-        scanf("%c", &a);
-        printf("------------------%d", i);
+        scanf(" %c", &a);
+        // printf("------------------%d", i);
         printf("Enter the value of b\n");
-        printf("------------------**********%d", i);
-        scanf("%c", &b);
+        // printf("------------------**********%d", i);
+        scanf(" %c", &b);
 
         printf("Enter the value of c\n");
-        scanf("%c", &c);
+        scanf(" %c", &c);
 
         i = i + 1;
     }
