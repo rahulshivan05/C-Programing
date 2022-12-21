e226b44f4872f55260665bc6acf40ae2fd3a0f1a
Rahul Kumar Gupta <rahulshivan05@gmail.com>
2022-12-21 17:23:49 +0530

    Conditions, if-else, Switch Case in C

---
+++ condition-or-loop.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char const *argv[])
+{
+    // bool my_bool = true;
+
+    int choice;
+
+    // printf("Enter a number: \n");
+    // scanf("%d", &choice);
+
+    // if (choice < 100)
+    // {
+    //     printf("Your input is less than a Hundred!\n");
+    //     if (choice < 50)
+    //     {
+    //         printf("Your input is also less than 50!\n");
+    //     }
+    // }
+
+    // if (choice % 2 == 0)
+    // {
+    //     printf("Your number is even!\n");
+    // }
+    // else
+    // {
+    //     printf("Your input is more than a Hundred!\n");
+    // }
+
+    // Switch Case statement
+
+    // switch (choice)
+    // {
+    // case 10:
+    //     printf("The input value is 10!\n");
+    //     break;
+
+    // case 100:
+    //     printf("The input value is 100!\n");
+    //     break;
+
+    // default:
+    //     printf("Out of the range\n");
+    //     break;
+    // }
+
+    bool myCondition = true;
+
+    printf(myCondition ? "True\n" : "False\n");
+
+    return 0;
+}
